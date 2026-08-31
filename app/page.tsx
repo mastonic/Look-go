de69c884e07d5447f3ae2010930ec8aa4807f9bd
mastonic <rigahludovic@gmail.com>
2026-08-31 15:20:01 +0200

    style: load corrected landing imagery presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { LookGoLandingV2 } from "@/components/landing/LookGoLandingV2";
 import "./lookgo-v2.css";
+import "./lookgo-v2-fixes.css";
 
 export default function HomePage(){
   return <LookGoLandingV2/>;
