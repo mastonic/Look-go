05d931907acb9f6afe1d74f548030f4fc7cc4c9b
mastonic <rigahludovic@gmail.com>
2026-08-31 14:58:34 +0200

    style: load landing motion and icon layer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { LookGoLanding } from "@/components/landing/LookGoLanding";
 import "./lookgo-2026.css";
+import "./lookgo-motion-icons.css";
 
 export default function HomePage(){
   return <LookGoLanding/>;
