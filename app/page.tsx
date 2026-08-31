f7a2ecda42fe9c9d35f29e2b520a194b712e7925
mastonic <rigahludovic@gmail.com>
2026-08-31 15:45:59 +0200

    style: activate approved hero reference treatment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { LookGoLandingV2 } from "@/components/landing/LookGoLandingV2";
 import "./lookgo-v2.css";
 import "./lookgo-v2-fixes.css";
 import "./lookgo-v3-overrides.css";
+import "./lookgo-reference-hero.css";
 
 export default function HomePage(){
   return <LookGoLandingV2/>;
