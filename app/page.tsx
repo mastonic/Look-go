9cf7f30df0fa8233e48d969c4ece1106dbc72cd5
mastonic <rigahludovic@gmail.com>
2026-08-31 16:37:20 +0200

    style: activate final Look&Go polish

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import "./lookgo-v3-overrides.css";
 import "./lookgo-reference-hero.css";
 import "./lookgo-v4-experience.css";
 import "./lookgo-v5-mobile-polish.css";
+import "./lookgo-v6-final-polish.css";
 
 export default function HomePage(){
   return <LookGoLandingV2/>;
