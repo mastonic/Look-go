6de80338dae8d2535e315febe256bc766aea3abb
mastonic <rigahludovic@gmail.com>
2026-08-31 15:39:38 +0200

    style: activate premium landing v3 overrides

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { LookGoLandingV2 } from "@/components/landing/LookGoLandingV2";
 import "./lookgo-v2.css";
 import "./lookgo-v2-fixes.css";
+import "./lookgo-v3-overrides.css";
 
 export default function HomePage(){
   return <LookGoLandingV2/>;
