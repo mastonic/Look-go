bcc40ae40f8bd084a5d5d9b1ee435cbc442d5976
mastonic <rigahludovic@gmail.com>
2026-08-31 15:53:38 +0200

    feat: activate connected wardrobe journey design

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import "./lookgo-v2.css";
 import "./lookgo-v2-fixes.css";
 import "./lookgo-v3-overrides.css";
 import "./lookgo-reference-hero.css";
+import "./lookgo-v4-experience.css";
 
 export default function HomePage(){
   return <LookGoLandingV2/>;
